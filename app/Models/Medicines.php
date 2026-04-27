1e749698cc41d98e5c4470af918e7a66ea6804d1
booleankris <booleankris@gmail.com>
2026-04-27 20:22:29 +0800

    Sync | Front-End & Backend | Medicines table change 2

--- app/Models/Medicines.php
+++ app/Models/Medicines.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Medicines extends Model
 {
     use HasFactory;
-    protected $table = 'medicinesnew';
+    protected $table = 'medicines';
     protected $fillable = [
         'code',
         'barcode',
