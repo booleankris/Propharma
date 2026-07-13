b26a726720bf9315611eb8e34913b42f14b53e10
booleankris <booleankris@gmail.com>
2026-07-13 17:46:14 +0800

    Sales Data | Front-End & Back-End | Receipt

--- resources/views/sales/salesdata.blade.php
+++ resources/views/sales/salesdata.blade.php
@@ -80,11 +80,9 @@
             color: #111827;
             border-bottom: 0.5px solid #f3f4f6 !important;
         }
-
-        #table-data tbody td {
-            text-align: center !important;
+        #table-data tbody td{
+            text-align: center!important;
         }
-
         #table-data tbody tr,
         #items-table tbody tr {
             cursor: pointer;
@@ -335,8 +333,8 @@
                                     <tr>
                                         <th>#</th>
                                         <th>Obat</th>
-                                        <th class="col-right">Qty</th>
                                         <th class="col-right">Harga</th>
+                                        <th class="col-right">Qty</th>
                                         <th class="col-right">Disc</th>
                                         <th class="col-right">Total</th>
                                     </tr>
@@ -503,14 +501,13 @@
                     {
                         data: 'medicine'
                     },
-
                     {
-                        data: 'quantity',
+                        data: 'price',
                         className: 'col-right',
                         render: val => `<span class="qty-badge">${val}</span>`
                     },
                     {
-                        data: 'price',
+                        data: 'quantity',
                         className: 'col-right',
                         render: val => `<span class="qty-badge">${val}</span>`
                     },
