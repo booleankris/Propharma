1d41233e195e89a28ee00f9f27873d66504a8d9c
booleankris <booleankris@gmail.com>
2026-08-03 00:31:17 +0800

    Orders & Receiving | Frontend & Backend | fix orders

--- resources/views/orders/printSPB.blade.php
+++ resources/views/orders/printSPB.blade.php
@@ -160,7 +160,7 @@
                     </tr>
                     <tr>
                         <td></td>
-                        <td style="text-align:right;font-size:6.5px;">
+                        <td style="text-align:right;font-size:6px;">
                             Di- {{ optional($items->first()->creditors)->address ?? '-' }}
                         </td>
                     </tr>
