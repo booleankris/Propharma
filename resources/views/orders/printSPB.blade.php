56a833bc20f5be93621c1567ffe91a2f0d9f2c4b
booleankris <booleankris@gmail.com>
2026-08-04 06:20:47 +0800

    Orders & Receiving | Frontend & Backend | fix orders

--- resources/views/orders/printSPB.blade.php
+++ resources/views/orders/printSPB.blade.php
@@ -126,7 +126,7 @@
                     <tr>
                         <td style="width:38px; vertical-align:middle;">
                             @if ($pharmacy->logo && file_exists(public_path('img/'.$pharmacy->logo)))
-                                <img src="{{ public_path('img/'.$pharmacy->logo) }}" style="width:40px;">
+                                <img src="{{ public_path('img/'.$pharmacy->logo) }}" style="width:35px; margin:0 20px;">
                             @else
                                 <img src="{{ public_path('img/logo-sahabat.png') }}" style="width:20px;">
                             @endif
